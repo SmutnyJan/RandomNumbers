8b6314686bbb76eeb7643465ec037c2aabfc24b0
Jan Smutný <u18.426@pslib.cloud>
2020-02-21 18:01:08 +0100

    commit

--- RandomNumbers/Game.cs
+++ RandomNumbers/Game.cs
@@ -8,23 +8,29 @@ namespace RandomNumbers
     {
         private Random _rand;
         private int _number;
+        private int _bottomrange;
+        private int _toprange;
+        public int count { get; private set; }
 
         public Game(int bottomrange, int toprange)
         {
-            _range = range;
-            _number = _rand.Next(bottomrange, toprange);
+            _rand = new Random();
+            _bottomrange = bottomrange;
+            _toprange = toprange;
+            _number = _rand.Next(_bottomrange, _toprange);
         }
 
         public GuessResult Guess(int number)
         {
+            count++;
             if (number > _number) return GuessResult.IsGreater;
             if (number < _number) return GuessResult.IsLower;
             else return GuessResult.Success;
         }
 
-        public void Restart()
-        {
-            Guess(25);
-        }
+        //public void Restart()
+        //{
+        //    Guess(25);
+        //}
     }
 }
--- RandomNumbers/Program.cs
+++ RandomNumbers/Program.cs
@@ -8,12 +8,35 @@ namespace RandomNumbers
         static void Main(string[] args)
         {
             Stopwatch sw = new Stopwatch();
-            bool uhodnto = false;
             bool pokracovat = true;
+            int cislo = 0;
             while (pokracovat)
             {
+                bool uhodnuto = false;
                 Game game = new Game(0, 10);
-                Console.WriteLine("IDK");
+                GuessResult guess; 
+                while (uhodnuto == false) { 
+                    Console.WriteLine("Zadej číslo: ");
+                    sw.Start();
+                    cislo = int.Parse(Console.ReadLine());
+                    sw.Stop();
+                    guess = game.Guess(cislo);
+                    if (guess == GuessResult.IsLower) {
+                        Console.WriteLine("Zadej větší");
+                    }
+                    else if (guess == GuessResult.IsGreater) {                 
+                        Console.WriteLine("Zadej menší");
+                    }
+                    else { 
+                        Console.WriteLine("Uhodnuto"); 
+                        uhodnuto = true;
+                    }
+                }
+                TimeSpan ts = sw.Elapsed;
+                Console.WriteLine("Hledané číslo bylo {0}", cislo);
+                Console.WriteLine("Počet pokusů: {0}", game.count);
+                Console.WriteLine("Čas: {0}", ts.Seconds);
+                
             }
         }
     }
